8a365986dddd9198d9bebba9318cfedf89007882
Chris Dorward <listingslab@gmail.com>
2017-04-04 10:12:54 +0930

    add .gitignore

---
+++ listingslab-plugin/listingslab-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Listingslab Plugin
+Plugin URI: http://listingslab.com/listingslab-plugin
+Description: Introduces functionality specific to the listingslab namespace.
+Author: Chris Dorward
+Version: 5.6
+Author URI: http://listingslab.com
+*/
+
+function safe_print() {
+ print " -------- I think I'm getting a clue!";
+}
+
+/* End of File */
