ed745f45a6dedf63331b2cbe481afd7f1833823f
Chris Dorward <listingslab@gmail.com>
2017-04-04 13:27:19 +0930

    Combine plugin option with front end link colour

--- listingslab-plugin/listingslab-plugin.php
+++ listingslab-plugin/listingslab-plugin.php
@@ -1,17 +1,62 @@
 <?php
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+?>
+<?php
 /*
 Plugin Name: Listingslab Plugin
 Plugin URI: http://listingslab.com/listingslab-plugin
-Description: Introduces functionality specific to the listingslab namespace.
+Description: Introduces functionality specific to the listingslab namespace. Including shortcodes and API endpoints
 Author: Chris Dorward
-Version: 5.6
+Version: 5.8
 Author URI: http://listingslab.com
 */
 
-function safe_print() {
- print " -------- I think I'm getting a clue!";
-}
-
 // [LL-Shortcode]
+include 'shortcodes/LL-Shortcode.php';
 
-/* End of File */
+
+add_action('admin_menu', 'listingslab_create_menu');
+function listingslab_create_menu() {
+	add_menu_page(
+		'Listingslab Settings',
+		'Listingslab',
+		'administrator',
+		__FILE__,
+		'listingslab_plugin_settings_page',
+		plugins_url('/images/iconMenu.png', __FILE__),
+		0
+	);
+	add_action( 'admin_init', 'register_listingslab_variables' );
+}
+function register_listingslab_variables() {
+	register_setting( 'listingslab-settings', 'listingslab_link_colour' );
+}
+function listingslab_plugin_settings_page() {
+?>
+	<div class="wrap">
+	<h1><img
+		alt="Listingslab"
+		src="<?php print plugins_url('/images/listingslab.png', __FILE__) ?>"
+		width="35"
+		align="bottom"
+	/> Listingslab Plugin</h1>
+	<form method="post" action="options.php">
+	    <?php settings_fields( 'listingslab-settings' ); ?>
+	    <?php do_settings_sections( 'listingslab-settings' ); ?>
+	    <table class="form-table">
+	        <tr valign="top">
+	        <th scope="row">Link colour</th>
+	        <td>#
+						<input
+							type="text"
+							name="listingslab_link_colour"
+							value="<?php echo esc_attr( get_option('listingslab_link_colour') ); ?>"
+						/>
+					</td>
+	        </tr>
+	    </table>
+	    <?php submit_button(); ?>
+	</form>
+	</div>
+<?php } ?>
--- listingslab-plugin/shortcodes/LL-Shortcode.php
+++ listingslab-plugin/shortcodes/LL-Shortcode.php
@@ -1,8 +1,17 @@
 <?php
 // [LL-Shortcode]
 
-function ll_shortcode() {
- print " -------- You should see this if all is well";
+function ll_shortcode( $atts, $content = null)	{
+
+	extract( shortcode_atts( array(
+				'message' => 'LL Shortcode Rendered'
+			), $atts
+		)
+	);
+	// this will display our message before the content of the shortcode
+	return $message . ' ' . $content;
+
 }
+add_shortcode('LL-Shortcode', 'll_shortcode');
 
 /* End of File */
